c2b55f8615f05d8af601aa44013e9bbac97d76ea
PepperWorm <168024258+PepperWorm@users.noreply.github.com>
2024-04-25 13:16:49 +0300

    completed the task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Util.getConnect();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.cleanUsersTable();
+
+        userDao.saveUser("User1", "LastName1", (byte) 19);
+        userDao.saveUser("User2", "LastName2", (byte) 33);
+        userDao.saveUser("User3", "LastName3", (byte) 24);
+        userDao.saveUser("User4", "LastName4", (byte) 45);
+
+        userDao.getAllUsers().forEach(System.out::println);
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+    public static Connection getConnect() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Соединение с БД установлено");
+            }
+        } catch (SQLException e) {
+            System.err.println("не удалось загрузить драйвер");
+        }
+        return connection;
+    }
 }
